a6ca888d91a607def6795a71dd52e2f9134e75bf
Arwa <arwa.it15@gmail.com>
2017-09-01 11:31:32 -0500

    limit to ten

--- limit-ten.js
+++ limit-ten.js
@@ -2,7 +2,7 @@ const { Transform } = require('stream');
 
 let count = 0;
 module.exports = () => Transform({
-    _transform(word, _, done) {
+    transform(word, _, done) {
         count ++;
         count <= 10 ? done(null, word) : process.exit();
     }
